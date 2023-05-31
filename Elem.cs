12b92fd06b4197be00640173aed905b655617c09
Géczi Kornél <hungarokornel@gmail.com>
2023-05-31 09:34:14 +0200

    Félidő

---
+++ Elem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Elem
+    {
+        private string year;
+        private string name;
+        private string sign;
+        private string plateNumber;
+        private string explorer;
+
+        public Elem(string year, string name, string sign, string plateNumber, string explorer)
+        {
+            this.year = year;
+            this.name = name;
+            this.sign = sign;
+            this.plateNumber = plateNumber;
+            this.explorer = explorer;
+        }
+
+        public string Year { get => year; }
+        public string Name { get => name; }
+        public string Sign { get => sign; }
+        public string PlateNumber { get => plateNumber; }
+        public string Explorer { get => explorer; }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,14 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp1
 {
     class Program
     {
+        static List<Elem> elems = new List<Elem>();
         static void Main(string[] args)
         {
-            Console.WriteLine("asd");
+            StreamReader sr = new StreamReader("felfedezesek.csv");
+            while(!sr.EndOfStream)
+            {
+                var data = sr.ReadLine().Split(';');
+                Elem elem = new Elem(data[0], data[1], data[2], data[3], data[4]);
+                elems.Add(elem);
+            }
+            sr.Close();
+            elems = elems.Skip(1).ToList();
+
+            Console.WriteLine($"3. feladat: Elemek száma: {elems.Count()}");
+            Console.WriteLine($"4. feladat: Felfedezések száma az ókorban: {elems.Where(x => x.Year == "Ókor").Count()}");
+            string sign;
+            while (true)
+            {
+                Console.Write("5. feladat: Kérek egy vegyjelet: ");
+                sign = Console.ReadLine();
+                if(sign.ToLower().Length <= 2)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("6. feladat: Keresés");
+            Elem foundElem = elems.First(x => x.Sign == sign);
+            if(foundElem != null)
+            {
+                Console.WriteLine($"\tAz elem vegyjele: {foundElem.Sign}");
+                Console.WriteLine($"\tAz elem neve: {foundElem.Name}");
+                Console.WriteLine($"\tRendszáma: {foundElem.PlateNumber}");
+                Console.WriteLine($"\tFelfedezés éve: {foundElem.Year}");
+                Console.WriteLine($"\tFelfedező: {foundElem.Explorer}");
+            }
+            else
+            {
+                Console.WriteLine("\tNincs ilyen elem az adatforrásban!");
+            }
         }
     }
 }
